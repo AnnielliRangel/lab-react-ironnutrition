85a8bf4e2e20d8d0836037549cea71f1a5d0340d
Annielli Rangel <anniellirangel@gmail.com>
2022-11-10 01:22:35 -0300

    solved lab, or not

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import foods from "./foods.json"
+
 import './App.css';
-import {useState} from "react"
+
+import FoodBox from './Components/FoodBox';
 
 function App() {
+  
 
-  const [allFoods, setAllFoods]= useState(foods)
-
-  return (<div className="App">
-
-    
-  </div>);
+  return (
+    <div className="App">
+      <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      />
+    </div>
+  );
 }
 export default App;
---
+++ src/Components/FoodBox.js
@@ -0,0 +1,32 @@
+import { Card, Col, Button } from 'antd';
+
+// Iteration 2
+function FoodBox({ foods }) {
+  return (
+    <div>
+      {foods.map((food) => {
+        return (
+          <Col>
+            <Card
+              title={food.name}
+              style={{ width: 230, height: 300, margin: 10 }}
+            >
+              <img src={food.image} height={60} alt="food" />
+              <p>Calories: {food.calories}</p>
+              <p>Servings: {food.servings}</p>
+              <p>
+                <b>
+                  Total Calories: {food.calories} * {food.servings}{' '}
+                </b>{' '}
+                kcal
+              </p>
+              <Button type="primary"> Delete </Button>
+            </Card>
+          </Col>
+        );
+      })}
+    </div>
+  );
+}
+
+export default FoodBox;
